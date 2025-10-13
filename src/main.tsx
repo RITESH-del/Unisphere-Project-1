163621360e7066bb1ec27e4e787f5e46f4783d1b
Ritesh-Kumar546 <ritesh.kumar.24cse@bmu.edu.in>
2025-10-13 19:32:33 +0530

    page-1

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import Root from "./routes/root.tsx";
+import HomePage from './routes/LibraryAdminHome.tsx';
 
 
 const router = createBrowserRouter([{
   path:"/",
   element: <Root />,
+  children:[{index: true, element: <HomePage />,},
+    
+  ]
 }])
 
 createRoot(document.getElementById('root')!).render(
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,16 +1,16 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider} from "@/components/ui/sidebar"
 import {AppSidebar} from '../components/app-sidebar';
 import {Outlet} from 'react-router-dom';
 
 function Root() {
     return (
         <>
-    <SidebarProvider className="border-r-8">
+    <SidebarProvider className="border-r-8 m-0 p-0">
       <AppSidebar />
         {/* <SidebarTrigger /> */}
-      <>
+      <div>
         <Outlet />
-      </>
+      </div>
     </SidebarProvider>
         </>
     )
